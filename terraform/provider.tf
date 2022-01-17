7cd4e70bb95cd4ecc426e52d2ad0e33d75e8dcd8
yubazhenov <bazhenovyuriy@mail.ru>
2022-01-17 13:14:00 -0800

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,8 @@
 provider "yandex" {
   cloud_id  = var.yc_cloud
   folder_id = var.yc_folder
-  service_account_key_file = "./key.json"
+  service_account_key_file = "./key.json" # путь к файлу с ключами сервисного аккаунта
+  #token = var.yc_token  не нужна, т.к. используем сервисный аккаунт
 }
 
 terraform {
